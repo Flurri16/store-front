2a3f81d16bc48fb8fb906ccc1479f7db7875634a
flurri16 <olezhakostov@gmail.com>
2025-06-09 20:30:08 +0200

    test 5

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: 'store-back-production-d6cb.up.railway.app/api'});
+const instance = axios.create({baseURL: 'https://store-back-production-d6cb.up.railway.app/api'});
 
 instance.interceptors.request.use((config) => {
     const token = window.localStorage.getItem('token')
