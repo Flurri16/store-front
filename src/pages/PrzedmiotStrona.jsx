9fbb231c6396aa757225671d23980eef61e1bf65
flurri16 <olezhakostov@gmail.com>
2025-07-22 10:16:09 +0200

    test 5325234r234

--- src/pages/PrzedmiotStrona.jsx
+++ src/pages/PrzedmiotStrona.jsx
@@ -55,7 +55,7 @@ export default function PrzedmiotStrona() {
             <div className="strona-left">
                 {
                     item && item.imagesPaths ? (
-                        <img src={`http://localhost:5000${item.imagesPaths[currentImageIndex]}`} alt="" />
+                        <img src={`https://store-back-9j9m.onrender.com${item.imagesPaths[currentImageIndex]}`} alt="" />
                     ) : null
                 }
                 {
